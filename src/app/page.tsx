8ce2fac01a307b0731410c274250286289ba1b4f
Brian Griffin <wbgriffin@pm.me>
2023-07-25 07:51:43 -0400

    feat: start suggestions page layout with sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,29 @@
-import Button from "@/components/Button"
+import styles from './HomePage.module.scss'
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main>
-      <p>Whereas disregard and contempt for human rights have resulted</p>
-      <Button
-        style="primary"
-        text="My Button"
-        back={true}
-      />
+      <header className={styles.header} data-menu="open">
+        <div className={styles.banner}>
+          <div>
+            <h1>Frontend Mentor</h1>
+            <p>Feedback Board</p>
+          </div>
+          <span>menu</span>
+        </div>
+        <aside className={styles.menu}>
+          <div className={styles['menu-inner']}>
+            <div>categories</div>
+            <div>roadmap</div>
+          </div>
+        </aside>
+      </header>
+      <div className={styles['suggestions-bar']}>
+        insert SuggestionsBar component here
+      </div>
+      <div className={styles['suggestions-list']}>
+        insert SuggestionsList component here
+      </div>
     </main>
   )
 }
