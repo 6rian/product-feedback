f0696cf6e7f37a14d9bf8a36eb450a3412fecf55
Brian Griffin <wbgriffin@pm.me>
2023-08-08 22:25:39 -0400

    feat: add categories menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,25 @@
 import Banner from '@/components/Banner'
+import Pill from '@/components/Pill'
 import SuggestionsBar from '@/components/SuggestionsBar'
 import styles from './HomePage.module.scss'
 
 export default function HomePage() {
+
+  const categories = ['All', 'UI', 'UX', 'Enhancement', 'Bug', 'Feature']
+  const categoryPills = categories.map((c, idx) => <Pill text={c} active={idx < 1 ? true : false} />)
+
   return (
     <main className={styles.main}>
       <header className={styles.header} data-menu="closed">
         <Banner />
         <aside className={styles.menu}>
           <div className={styles['menu-inner']}>
-            <div>categories</div>
-            <div>roadmap</div>
+            <div className={styles.categories}>{categoryPills}</div>
+            {/* <div>roadmap</div> */}
           </div>
         </aside>
       </header>
       <div>
-        {/* <div className={styles['suggestions-bar']}> */}
-        {/*   insert SuggestionsBar component here */}
-        {/* </div> */}
         <SuggestionsBar />
         <div className={styles['suggestions-list']}>
           insert SuggestionsList component here
